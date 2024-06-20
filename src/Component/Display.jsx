d9a7354c46cc2c2a8791f9c1d92210a7db56c432
himani bhimate <himanibhimate0808@gmail.com>
2024-06-20 22:34:44 +0530

    change in homecss

--- src/Component/Display.jsx
+++ src/Component/Display.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Table from 'react-bootstrap/Table';
-import { Link } from "react-router-dom";
 import "../assets/css/display.css"; 
 
 const Display = () => {
